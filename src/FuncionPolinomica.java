80e80159eab567efb886209a390a75253aaca0c6
imfloopa <0512025073@alumnos.unp.edu.pe>
2026-06-06 12:37:24 -0500

    Agregué las variables en las que se almacenan los limites y el numero de trapecios

--- src/FuncionPolinomica.java
+++ src/FuncionPolinomica.java
@@ -1,8 +1,13 @@
 public class FuncionPolinomica {
     private double[] coeficientes;
-
-    public FuncionPolinomica(double[] coeficientes) {
+    private int numeroDeTrapecios;
+    private float[] limites;
+    
+//constructor de la clase FuncionPolinomica
+    public FuncionPolinomica(double[] coeficientes, int numeroDeTrapecios, float[] limites) {
         this.coeficientes = coeficientes;
+        this.numeroDeTrapecios = numeroDeTrapecios;
+        this.limites = limites;
     }
 // Esta función evalua el valor de la funcion para un valor de x
     public double evaluar(double x) {
