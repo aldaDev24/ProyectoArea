0265604f06b0aa8bd1f51852c112a538d89e32f1
imfloopa <0512025073@alumnos.unp.edu.pe>
2026-06-06 01:55:01 -0500

    Creación del método Evaluar () en la clase FuncionPolinomica

--- src/FuncionPolinomica.java
+++ src/FuncionPolinomica.java
@@ -1,2 +1,16 @@
 public class FuncionPolinomica {
+    private double[] coeficientes;
+
+    public FuncionPolinomica(double[] coeficientes) {
+        this.coeficientes = coeficientes;
+    }
+// Esta función evalua el valor de la funcion para un valor de x
+    public double evaluar(double x) {
+        double resultado = 0.0;
+        for (int i = 0; i < coeficientes.length; i++) {
+            resultado += coeficientes[i] * Math.pow(x, i);
+        }
+        return resultado;
+    }
+
 }
