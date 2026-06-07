6212a5244925b5b3174498a955d16a8cb5665a15
Nima Erazo <0512025074@alumnos.unp.edu.pe>
2026-06-06 22:56:24 -0500

    implementacion de la clase Metodo Trapecio

--- src/MetodoTrapecio.java
+++ src/MetodoTrapecio.java
@@ -1,13 +1,16 @@
 public class MetodoTrapecio {
-    public double calcularArea(double a, double b,int n, FuncionPolinomica funcion) {
-          double ancho=(a+b)/(int) n ;
-          double suma=funcion.evaluar(a)+funcion.evaluar(b);
-          for(int i=1;i<n;i++){
-              double xactual=a+(i*ancho);
 
-              suma += 2.0*funcion.evaluar(xactual);
-          }
-          return (ancho/2.0)*suma;
+    public double calcularArea(double a, double b, int n, FuncionPolinomica funcion) {
 
+        double deltaX = (b - a) / (double) n;
+
+        double suma = funcion.evaluar(a) + funcion.evaluar(b);
+
+        for (int i = 1; i < n; i++) {
+            double xActual = a + (i * deltaX);
+            suma += 2.0 * funcion.evaluar(xActual);
+        }
+
+        return (deltaX / 2.0) * suma;
     }
 }
