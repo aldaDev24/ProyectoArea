0c2bbccfa7fcc98feff675276f944ab6c46aabde
imfloopa <0512025073@alumnos.unp.edu.pe>
2026-06-07 00:19:09 -0500

    Eliminacion de metodos y atributos redundantes

--- src/FuncionPolinomica.java
+++ src/FuncionPolinomica.java
@@ -1,36 +1,9 @@
 public class FuncionPolinomica {
      public double[] coeficientes;
-     public int numeroDeTrapecios;
-     public float[] limites;
-
-    public void setCoeficientes(double[] coeficientes) {
-        this.coeficientes = coeficientes;
-    }
-    public void setNumeroDeTrapecios(int numeroDeTrapecios) {
-        this.numeroDeTrapecios = numeroDeTrapecios;
-    }
-    public void setLimites(float[] limites) {
-        this.limites = limites;
-    }
-
-    public double[] getCoeficientes() {
-        return coeficientes;
-    }
-    public int getNumeroDeTrapecios() {        
-        return numeroDeTrapecios;
-    }
-    public int getLimiteInferior() {
-        return (int) limites[0];
-    }
-    public int getLimiteSuperior() {
-        return (int) limites[1];
-    }
 
 //constructor de la clase FuncionPolinomica
-    public FuncionPolinomica(double[] coeficientes, int numeroDeTrapecios, float[] limites) {
+    public FuncionPolinomica(double[] coeficientes) {
         this.coeficientes = coeficientes;
-        this.numeroDeTrapecios = numeroDeTrapecios;
-        this.limites = limites;
     }
 // Esta función evalua el valor de la funcion para un valor de x
     public double evaluar(double x) {
