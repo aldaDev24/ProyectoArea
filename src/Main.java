c21e00dd16a356547caa68ea8d843219306967d7
AldaDev <alddev2007@gmail.com>
2026-06-06 19:13:09 -0500

    Realize el esqueleto de la clase Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+
+        // Leer datos
+
+        // Validar datos
+
+        // Crear funcion
+
+        // Calcular area
+
+        // Mostrar resultado
+
+    }
 }
