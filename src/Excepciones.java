84c26598dad4b5bced85aae546e2d8a5df8d1616
AldaDev <alddev2007@gmail.com>
2026-06-06 20:07:27 -0500

    Implementadas validaciones para trapecios y funciones polinomicas

--- src/Excepciones.java
+++ src/Excepciones.java
@@ -1,2 +1,30 @@
 public class Excepciones {
+    public static void validarNumeroTrapecios(int n){
+        if(n<=0){
+            throw new IllegalArgumentException(
+                    "El numero de trapecios debe ser mayor que cero"
+            );
+        }
+    }
+
+    public static void validarFuncion(Double[] coeficientes){
+        if (coeficientes == null) {
+            throw new IllegalArgumentException(
+                    "Los coeficientes no pueden ser nulos"
+            );
+        }
+
+        if (coeficientes.length < 3){
+            throw new IllegalArgumentException(
+                    "La funcion debe ser de grado 2 como minimo"
+            );
+        }
+
+        if (coeficientes[0].equals(0.0)){
+            throw new IllegalArgumentException(
+                    "El coeficiente del termino cuadratico no puede ser cero"
+            );
+        }
+    }
+
 }
