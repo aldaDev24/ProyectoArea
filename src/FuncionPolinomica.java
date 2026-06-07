fc3017f9ff78f7d2c021315ce1f0b1de86f35229
imfloopa <0512025073@alumnos.unp.edu.pe>
2026-06-07 00:39:46 -0500

    Eliminacion de elementos redundantes

--- src/FuncionPolinomica.java
+++ src/FuncionPolinomica.java
@@ -1,19 +1,16 @@
 public class FuncionPolinomica {
-     public double[] coeficientes;
+    public double[] coeficientes;
 
 //constructor de la clase FuncionPolinomica
     public FuncionPolinomica(double[] coeficientes) {
         this.coeficientes = coeficientes;
     }
 
-    public double[] getCoeficientes() {
-        return coeficientes;
-    }
 // Esta función evalua el valor de la funcion para un valor de x
     public double evaluar(double x) {
         double resultado = 0.0;
         for (int i = 0; i < coeficientes.length; i++) {
-            resultado += coeficientes[i] * Math.pow(x, i);
+            resultado += coeficientes[coeficientes.length - 1 - i] * Math.pow(x, coeficientes.length - 1 - i);
         }
         return resultado;
     }
