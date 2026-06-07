7ff6603957427eb914e3eee0a08c68c4f319170e
Nima Erazo <0512025074@alumnos.unp.edu.pe>
2026-06-06 22:34:58 -0500

    .

--- src/MetodoTrapecio.java
+++ src/MetodoTrapecio.java
@@ -1,2 +1,13 @@
 public class MetodoTrapecio {
+    public double calcularArea(double a, double b,int n, FuncionPolinomica funcion) {
+          double ancho=(a+b)/(int) n ;
+          double suma=funcion.evaluar(a)+funcion.evaluar(b);
+          for(int i=1;i<n;i++){
+              double xactual=a+(i*ancho);
+
+              suma += 2.0*funcion.evaluar(xactual);
+          }
+          return (ancho/2.0)*suma;
+
+    }
 }
